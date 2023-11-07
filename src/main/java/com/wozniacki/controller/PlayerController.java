a6218b6c0588848c086539734b6cfe95fb5eeadd
wozniackia <wozniackia@gmail.com>
2023-11-07 01:03:42 +0100

    Deploy #16

--- src/main/java/com/wozniacki/controller/PlayerController.java
+++ src/main/java/com/wozniacki/controller/PlayerController.java
@@ -6,22 +6,26 @@ import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.QueryValue;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Optional;
 
 @Controller("api/v1/player")
 @RequiredArgsConstructor
+@Slf4j
 public class PlayerController {
 
     private final PlayerRepository playerRepository;
 
     @Get
     public Iterable<Player> getAllPlayers() {
+        log.info("Entered getAllPlayers");
         return playerRepository.findAll();
     }
 
     @Get("/top")
     public Iterable<Player> getTopPlayers(@QueryValue Optional<Integer> limit) {
+        log.info("Entered getTopPlayers");
         return playerRepository.findTopPlayers(limit.orElse(10));
     }
 }
