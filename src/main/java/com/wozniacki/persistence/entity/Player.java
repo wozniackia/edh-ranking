fc141d9d4a1ba48b872c78dbaa916600f7283c6f
wozniackia <wozniackia@gmail.com>
2023-11-09 23:39:43 +0100

    Player nickname nullable

--- src/main/java/com/wozniacki/persistence/entity/Player.java
+++ src/main/java/com/wozniacki/persistence/entity/Player.java
@@ -1,9 +1,12 @@
 package com.wozniacki.persistence.entity;
 
+import io.micronaut.core.annotation.NonNull;
 import io.micronaut.data.annotation.GeneratedValue;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
 import io.micronaut.serde.annotation.Serdeable;
+import jakarta.annotation.Nonnull;
+import jakarta.annotation.Nullable;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -17,8 +20,11 @@ public class Player {
     @Id
     @GeneratedValue
     private Integer id;
+    @Nullable
     private String nickname;
+    @Nonnull
     private String firstName;
+    @Nonnull
     private String lastName;
     private int matches = 0;
     private int wins = 0;
