4b3b1daec31aa057d964b73946e659e64a2f659d
wozniackia <wozniackia@gmail.com>
2023-11-16 05:11:48 +0100

    Migration v4

--- src/main/resources/db/migration/V4__player_auth.sql
+++ src/main/resources/db/migration/V4__player_auth.sql
@@ -1,3 +1,3 @@
 alter table player drop column nickname;
 alter table player add column username varchar(20) not null unique;
-alter table player add column password varchar(20) not null;
+alter table player add column password varchar(64) not null;
